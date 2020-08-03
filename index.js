8806d212f0004508a80a4793e916c65889002d63
AlenSaito1 <66544823+AlenSaito1@users.noreply.github.com>
2020-08-03 17:20:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fs = require('fs-extra');
 const moment = require('moment')
 const fbvid = require('fbvideos'); //npm install --save fbvideos
 const malScraper = require('mal-scraper');
-const anime_Name = 'Sakura Trick'
 var quote_Array = ["“You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.”– Dr. Suess", "“I’m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can’t handle me at my worst, then you sure as hell don’t deserve me at my best.”– Marilyn Monroe", "“Get busy living or get busy dying.”– Stephen King", '"Time moves in one direction, memory in another." \n~ William Gibson', '"The sky above the port was the color of television, tuned to a dead station." \n~ William Gibson', '"Before you diagnose yourself with depression or low self-esteem, first make sure that you are not, in fact, just surrounded by assholes." \n~ William Gibson', '"When you want to know how things really work, study them when they\'re coming apart." \n~ William Gibson', '"Anything that can be done to a rat can be done to a human being. And we can do most anything to rats. This is a hard thing to think about, but it\'s the truth. It won\'t go away because we cover our eyes. THAT is cyberpunk." \n~ Bruce Sterling', '"Japan is a wonderful country, a strange mixture of ancient mystique and cyberpunk saturation. It\'s a monolith of society\'s achievements, yet maintains a foothold in the past, creating an amazing backdrop for tourings and natives alive. Japan captures the imagination like no other. You never feel quite so far from home as you do in Japan, yet there are no other people on the planet that make you feel as comfortable." \n~ Corey Taylor', '“Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.” \n– Mark Twain', '“When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.” \n– Audre Lorde', '“Those who dare to fail miserably can achieve greatly.” \n– John F. Kennedy', '“Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world.” \n– Lucille Ball', '“It is our choices, that show what we truly are, far more than our abilities.”\n– J. K Rowling', '“If you want to be happy, be.” \n– Leo Tolstoy', '“If you want to live a happy life, tie it to a goal, not to people or things.” \n– Albert Einstein', '“I never knew how to worship until I knew how to love.” \n– Henry Ward Beecher', '“Life is trying things to see if they work.” \n– Ray Bradbury', '“If life were predictable it would cease to be life, and be without flavor.” \n– Eleanor Roosevelt', '“Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.” \n– Bil Keane', '“You miss 100 percent of the shots you never take.” \n– Wayne Gretzky', '“Always forgive your enemies; nothing annoys them so much.” \n– Oscar Wilde']
 
 
@@ -63,7 +62,7 @@ async function msgHandler (client, message) {
         const { id, pushname } = sender
         const { name } = chat
         const time = moment(t * 1000).format('DD/MM HH:mm:ss')
-        const commands = ['#sticker', '#TnC', '#You are idiot', '#you are idiot', '#you are baka', '#you are bakka', '#YOU ARE IDIOT', '#You are bakka','#quotes', "#Quotes", '#stiker', '#hello','#info','#commands','#God','#Thank you','#I love you','#Seasonal anime','fuck','Fuck','sex','Sex','nudes','Link','Zelda','#Best girl','#S-1','#Do you love me','#tsundere','#Tsundere','Ora ora ora ora','ora ora ora ora','Ora Ora Ora Ora','Muda Muda Muda Muda','muda muda muda muda','Muda muda muda muda','yo','freedom','Freedom','#Zelda Timeline','#Botw','I love Rem','i love rem','I Love Rem','i love Rem','El Psy Congroo','Tuturu','Indeed','Can you beat Goku though','Se no','Mou','Kokoro','#neko','#wallpaper','#source','#sauce','#fb','Heave ho','Heave ho!','Make me a coffee','#Mystery Video','Never gonna','never gonna','never gonna run around','#Pokemon','#waifu','#waifu','Mily x Yagu','#Pokewall','#pokewall']
+        const commands = ['#sticker', '#TnC', '#You are idiot', '#you are idiot', '#you are baka', '#you are bakka', '#YOU ARE IDIOT', '#You are bakka','#quotes', "#Quotes", '#stiker', '#hello','#info','#commands','#God','#Thank you','#I love you','#Seasonal anime','#anime','#Anime','fuck','Fuck','sex','Sex','nudes','Link','Zelda','#Best girl','#S-1','#Do you love me','#tsundere','#Tsundere','Ora ora ora ora','ora ora ora ora','Ora Ora Ora Ora','Muda Muda Muda Muda','muda muda muda muda','Muda muda muda muda','yo','freedom','Freedom','#Zelda Timeline','#Botw','I love Rem','i love rem','I Love Rem','i love Rem','El Psy Congroo','Tuturu','Indeed','Can you beat Goku though','Se no','Mou','Kokoro','#neko','#wallpaper','#source','#sauce','#fb','Heave ho','Heave ho!','Make me a coffee','#Mystery Video','Never gonna','never gonna','never gonna run around','#Pokemon','#waifu','#waifu','Mily x Yagu','#Pokewall','#pokewall']
         const cmds = commands.map(x => x + '\\b').join('|')
         const cmd = type === 'chat' ? body.match(new RegExp(cmds, 'gi')) : type === 'image' && caption ? caption.match(new RegExp(cmds, 'gi')) : ''
 
@@ -149,6 +148,14 @@ async function msgHandler (client, message) {
                     break;
                 case 'Link' :
                         client.sendText(from, 'Zelda')
+                case '#anime':
+                case '#Anime':
+                         const name = 'Sakura Trick'
+ 
+                        malScraper.getInfoFromName(name)
+                       .then((data) => console.log(data))
+                       .catch((err) => console.log(err))
+                        client.SendText(data)
                     break;
                case 'Can you beat Goku though' :
                         client.sendText(from, '*I can and I will*')
@@ -258,7 +265,7 @@ async function msgHandler (client, message) {
                         client.sendText(from, 'Ora Ora Ora Ora')
                     break;
         case '#commands':
-                        client.sendText(from, '👋️Hi there\n\nKey:\n\n#example \n-Function\n\n💻️Commands\n\n#sticker \nTurns sticker to images⛩️ \n\n#neko\nReturns a random cat image🐈️\n\n#Quotes or #quotes\nReturns a quote that will either give you existential crises or wisdom\n\n#Pokemon Displays picture of a random pokemon \n \n#wallpaper \nSends a random anime wallpapers (beta) 🌌️\n\n#Seasonal anime \nReturns a list of seasonal animes🌆️\n\n#info \nDisplays the the terms and conditions & info📒️\n\n *There are many hidden and fun keywords* ;)')
+                        client.sendText(from, '👋️Hi there\n\nKey:\n#example-command \n-Function\n\n\n💻️ Commands\n\n#sticker ⛩️\n-Turns sticker to images \n\n#neko 🐱\nReturns a random cat image\n\n#Quotes or #quotes 🏷️\n-Returns a quote that will either give you existential crises or wisdom\n\n#Pokemon 🌠\n-Displays picture of a random pokemon \n \n#wallpaper 🏞️\n-Sends a random anime wallpapers \n\n#Seasonal anime🗾\nReturns a list of seasonal animes\n\n#info 📔\n-Displays the information\n\n #TnC 🗂️\n-Displays the Terms and Conditions \n\nThere are many hidden and fun keywords ;)')
                     break;
         case '#Seasonal anime':
                         client.sendText(from, 'Summer 2020 \n Re:Zero kara Hajimeru Isekai Seikatsu 2nd Season \n Yahari Ore no Seishun Love Comedy wa Machigatteiru. Kan \n The God of High School \n Sword Art Online: Alicization - War of Underworld 2nd Season \n Enen no Shouboutai: Ni no Shou \n Maou Gakuin no Futekigousha: Shijou Saikyou no Maou no Shiso, Tensei shite Shison-tachi no Gakkou e \n Kanojo, Okarishimasu \n Deca-Dence \n Uzaki-chan wa Asobitai! \n Monster Musume no Oishasan')
@@ -270,7 +277,7 @@ async function msgHandler (client, message) {
                         client.sendText(from, 'This is an open-source program written in Javascript. \n \nBy using the bot you agreeing to our Terms and Conditions \n \nTerms and conditions \n \nYour texts and your whatsapp username will be stored on our servers as long as the bot is active, your data will be erased when the bot goes offline. We do NOT store the images, videos, audio files and documents you send. We will never ask you to sign up or ask you for any of your passwords, OTPs or PINs. \n \n Thank you, Have a great day! \n \n Learn More about the bot: https://bit.ly/39Ld2L8 \n \n - Developers');
                         break;
                 case '#info':
-                        client.sendText(from, '👋️Hi, I\'m Emilia.\nThis project is open source, built using Javascript || Node.js and is available at GitHub https://bit.ly/39Ld2L8. If you are willing to contribute to our project please refer to the mentioned url.\n *Developers*\n _Alen Yohannan_ & _Somnath Das_\n Have a great day! ')   
+                        client.sendText(from, '👋️Hi there, I\'m Emilia\nThis project is open source, built using Javascript || Node.js and is available at GitHub https:\/\/bit.ly\/39Ld2L8. If you are willing to contribute to our project please refer to the mentioned url.\n \n\nDevelopers✨\n \n _Alen Yohannan_ \n_Somnath Das_\n\nContributors💫\n\n_Miliana Blue_\n_Aman Sakuya_')   
                         break;
                 case 'Make me a coffee':
                         await client.simulateTyping(from, true)
